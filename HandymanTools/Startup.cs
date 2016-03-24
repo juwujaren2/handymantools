fb85f38a6961ab47bce51bd937412bdbda2afdfd
Peter Graening <peter.graening@live.com>
2016-03-23 21:40:40 -0400

    Initial commit - will clean this up A LOT

---
+++ HandymanTools/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HandymanTools.Startup))]
+namespace HandymanTools
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
