386bbf02f2c8fb3c72f9297562c235e1ad32d3ed
Peter Graening <peter.graening@live.com>
2016-04-04 16:07:16 -0400

    Beginnings of view models and tool repository

---
+++ HandymanTools.Infrastructure/Repositories/ToolRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HandymanTools.Common.Enums;
+using HandymanTools.Common.Models;
+
+namespace HandymanTools.Infrastructure.Repositories
+{
+    class ToolRepository
+    {
+        public List<Tool> CheckToolAvailability(ToolType toolType, DateTime startDate, DateTime endDate)
+        {
+            
+        }
+
+        public Tool GetToolInfo(int toolId)
+        {
+            
+        }
+
+        public bool SellTool(int toolId)
+        {
+            
+        }
+
+        public int AddTool(Tool tool)
+        {
+            
+        }
+    }
+}
