f55bbf78b73363a2506457ac3e8231ffa971b180
Peter Graening <peter.graening@live.com>
2016-04-07 00:59:11 -0400

    Make reservation and reservation summary are complete.  Confirmation is next.

--- HandymanTools/Controllers/ReservationController.cs
+++ HandymanTools/Controllers/ReservationController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http.ModelBinding;
+using System.Web.Mvc;
+using HandymanTools.Infrastructure.Repositories;
 
 namespace HandymanTools.Controllers
 {
@@ -8,6 +12,36 @@ namespace HandymanTools.Controllers
         public ActionResult Make()
         {
             return View();
+        }
+
+        public ActionResult Confirmation(FormCollection formData)
+        {
+            return View();
+        }
+
+        public ActionResult Summary(FormCollection formData)
+        {
+            var startDate = DateTime.Parse(formData["startDate"]);
+            var endDate = DateTime.Parse(formData["endDate"]);
+            ViewBag.StartDate = startDate.ToShortDateString();
+            ViewBag.EndDate = endDate.ToShortDateString();
+            var tools = new Dictionary<int, string>();
+            decimal totalRental = 0;
+            decimal totalDeposit = 0;
+            var ToolRepository = new ToolRepository();
+            for (int i = 0; i < formData.AllKeys.Length; i++)
+            {
+                if (formData.AllKeys[i].Contains("toolSelection"))
+                {
+                    var tool = ToolRepository.GetToolInfo(int.Parse(formData[i]));
+                    tools.Add(tool.ToolId, tool.AbbrDescription);
+                    totalRental += tool.RentalPrice;
+                    totalDeposit += tool.DepositAmount;
+                }
+            }
+            ViewBag.TotalRental = totalRental;
+            ViewBag.totalDeposit = totalDeposit;
+            return View(tools);
         }
     }
 }
