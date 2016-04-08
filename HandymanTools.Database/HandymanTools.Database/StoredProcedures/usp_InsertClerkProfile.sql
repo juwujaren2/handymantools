9af7b5cc25d1ceb8bf3b4cc797e3531bd4dcd0c5
Ram Vedam <vedam@gatech.edu>
2016-04-07 23:30:18 -0400

    Created a stored procedure for inserting clerks

---
+++ HandymanTools.Database/HandymanTools.Database/StoredProcedures/usp_InsertClerkProfile.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[usp_InsertClerkProfile]
+	@UserName varchar(36),
+	@FirstName varchar(100),
+    @LastName varchar(100),
+    @Password varchar(256),
+	@PasswordHash varchar(256)
+AS
+	INSERT INTO [User] (UserName, [Password], [PasswordHash], FirstName, LastName)
+	VALUES (@UserName, @Password, @PasswordHash, @FirstName, @LastName)
+
+	INSERT INTO Clerk (UserName)
+	VALUES (@UserName)
+
+RETURN 0
