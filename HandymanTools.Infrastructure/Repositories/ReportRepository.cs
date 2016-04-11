218aebea41cced8f619944f71916f5689f280fc8
Ram Vedam <vedam@gatech.edu>
2016-04-11 01:41:22 -0400

    Feature - Base Report Front-End and Models
    * Added InventoryItem as an Aggregate Object to keep track
    of database results from inventory report query.
    * Added ViewModel and basic front-end for all reports
    * Added Interface for Report Repository
    * Added implementation for Inventory Reports.

---
+++ HandymanTools.Infrastructure/Repositories/ReportRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HandymanTools.Common.Models;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace HandymanTools.Infrastructure.Repositories
+{
+    public class ReportRepository : IReportRepository
+    {
+        private string _connectionString;
+
+        public ReportRepository()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        public List<InventoryItem> GenerateInventoryReport()
+        {
+            var result = new List<InventoryItem>();
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                var inventoryCommand = new SqlCommand
+                {
+                    CommandType = CommandType.StoredProcedure,
+                    CommandText = @"usp_Report_Inventory",
+                    Connection = conn
+                };
+
+                var reader = inventoryCommand.ExecuteReader();
+
+                while(reader.HasRows)
+                {
+                    while(reader.Read())
+                    {
+                        var newItem = new InventoryItem();
+                        newItem.Tool = new Tool();
+                        newItem.Tool.ToolId = reader.GetInt32(0);
+                        newItem.Tool.AbbrDescription = reader.GetString(1);
+                        newItem.RentalProfit = reader.GetDecimal(2);
+                        newItem.CostOfRepairs = reader.GetDecimal(3);
+                        newItem.TotalProfit = reader.GetDecimal(4);
+                        result.Add(newItem);
+                    }
+                    reader.NextResult();
+                }
+
+                conn.Close();
+            }
+
+                return result;
+        }
+    }
+}
---
+++ HandymanTools/Models/ReportViewModel.cs
@@ -0,0 +1,17 @@
+﻿using HandymanTools.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HandymanTools.Models
+{
+    public class ReportViewModel
+    {
+        public List<InventoryItem> Inventory { get; set; }
+
+        //public List<ClerkProgressItem> ClerkProgress { get; set; }
+        
+        //public List<CustomerRentalItem> CustomerRental { get; set; }
+    }
+}
