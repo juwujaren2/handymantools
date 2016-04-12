b6e785d042a81cb51decd3707a79dbe4cb987e8f
Ram Vedam <vedam@gatech.edu>
2016-04-12 10:17:42 -0400

    New Models for Customer Rental Summary and Clerk Progress Report

---
+++ HandymanTools.Common/Models/CustomerRentalSummary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandymanTools.Common.Models
+{
+    public class CustomerRentalSummary
+    {
+        public Customer Customer { get; set; }
+
+        public int NumberOfRentals { get; set; }
+
+    }
+}
