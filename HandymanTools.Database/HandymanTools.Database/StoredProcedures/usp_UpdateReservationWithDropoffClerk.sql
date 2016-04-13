ab4da0451f0a9fb04a93ed7243edca8a375780fd
chiqua25 <juwujaren@gmail.com>
2016-04-13 01:48:46 -0400

    Feature: Add untracked files for UpdateReservationWithDropOffClerk, RentalReceipt.cshtml, and Create.cshtml

---
+++ HandymanTools.Database/HandymanTools.Database/StoredProcedures/usp_UpdateReservationWithDropoffClerk.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[usp_UpdateReservationWithDropoffClerk]
+	@ClerkId varchar(36),
+	@ReservationNumber int
+AS
+	UPDATE Reservation
+	SET DropoffClerkId = @ClerkId
+	WHERE ReservationNumber = @ReservationNumber
+	
+RETURN 0
