dfcdfb5909e82b407a9eab979ba55e055031020d
Peter Graening <peter.graening@live.com>
2016-04-14 23:19:02 -0400

    fixed layout and get tool availability SP

--- HandymanTools.Database/HandymanTools.Database/StoredProcedures/usp_GetToolAvailability.sql
+++ HandymanTools.Database/HandymanTools.Database/StoredProcedures/usp_GetToolAvailability.sql
@@ -3,11 +3,19 @@
 	@EndDate Date,
 	@ToolType varchar(25)
 AS
-SELECT t.ToolId, AbbrDescription, t.DepositAmt, RentalPrice 
+SELECT t.ToolId, t.AbbrDescription, t.DepositAmt, t.RentalPrice
 FROM Tool t
-	LEFT JOIN ReservationTool rt ON t.ToolId = rt.ToolId
-	LEFT JOIN Reservation r ON r.ReservationNumber = rt.ReservationNumber AND (DATEADD(DD, -1, r.EndDate)) >= @StartDate AND r.StartDate <= @EndDate
-	LEFT JOIN ServiceOrder s ON s.ToolId = t.ToolId AND s.EndDate >= @StartDate AND s.StartDate <= @EndDate
-WHERE s.ToolId IS NULL AND r.ReservationNumber IS NULL and t.ToolType = @ToolType AND t.SaleDate IS NULL
+WHERE ToolType = @ToolType
+AND ToolID NOT IN (
+SELECT DISTINCT t.ToolId
+FROM ReservationTool rt
+INNER JOIN Tool t on t.ToolId = rt.ToolId
+INNER JOIN Reservation r on r.ReservationNumber = rt.ReservationNumber
+WHERE (DATEADD(DD, -1, EndDate)) >= @StartDate AND StartDate <= @EndDate
+UNION
+SELECT DISTINCT so.ToolId
+FROM ServiceOrder so
+WHERE (DATEADD(DD, -1, EndDate)) >= @StartDate AND StartDate <= @EndDate)
+AND SaleDate IS NULL
 
 RETURN 0
