0287159de9c9b86683dfedb2447bf5196f87f67d
Ram Vedam <vedam@gatech.edu>
2016-04-16 13:02:47 -0400

    Fixed Inventory Report Bugs
    - Fixed issue with RentalProfit caused by incorrectly checking sales date
    existence instead of ignoring it.
    
    - Fixed duration calculation since subtraction is already an exclusive
    calculation.

--- HandymanTools.Database/HandymanTools.Database/StoredProcedures/usp_Report_Inventory.sql
+++ HandymanTools.Database/HandymanTools.Database/StoredProcedures/usp_Report_Inventory.sql
@@ -1,12 +1,12 @@
 ﻿CREATE PROCEDURE [dbo].[usp_Report_Inventory]
 AS
-	SELECT Tool.ToolId, Tool.AbbrDescription, ISNULL(RentalProfit, 0), ISNULL(CostOfRepairs, 0), 
+	SELECT Tool.ToolId, Tool.AbbrDescription, ISNULL(RentalProfit, 0) AS RentalProfit, ISNULL(CostOfRepairs, 0) AS CostOfRepairs, 
 	ISNULL((RentalProfit - CostOfRepairs), 0) AS TotalProfit FROM Tool
 	LEFT OUTER JOIN (
-		SELECT Tool.ToolID, SUM(RentalPrice * (DATEDIFF(dd, Reservation.StartDate, Reservation.EndDate) - 1)) AS RentalProfit FROM Tool 
+		SELECT Tool.ToolID, SUM(RentalPrice * DATEDIFF(dd, Reservation.StartDate, Reservation.EndDate)) AS RentalProfit FROM Tool 
 		INNER JOIN ReservationTool ON ReservationTool.ToolID = Tool.ToolID 
 		INNER JOIN Reservation ON ReservationTool.ReservationNumber = Reservation.ReservationNumber
-		WHERE Tool.SaleDate <> NULL GROUP BY Tool.ToolID
+		WHERE Tool.SaleDate IS NULL GROUP BY Tool.ToolID
 	) AS Profit ON Tool.ToolId = Profit.ToolID 
 	LEFT OUTER JOIN (
 		SELECT Tool.ToolId, SUM(ServiceOrder.EstimatedCost) AS CostOfRepairs FROM Tool 
