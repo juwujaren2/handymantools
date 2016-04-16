e87a05a12ca5dd153ad7e0788a4013353c106e1c
Ram Vedam <vedam@gatech.edu>
2016-04-16 19:48:56 -0400

    Fixed Clerk Progress Report bug where only total pickups where shown but not clerks that did just dropoffs

--- HandymanTools.Database/HandymanTools.Database/StoredProcedures/usp_Report_ClerkProgress.sql
+++ HandymanTools.Database/HandymanTools.Database/StoredProcedures/usp_Report_ClerkProgress.sql
@@ -5,13 +5,13 @@ AS
 	SELECT [User].FirstName, [User].LastName, ISNULL(TotalPickups, 0), ISNULL(TotalDropoffs, 0), (ISNULL(TotalPickups, 0) + ISNULL(TotalDropoffs, 0)) AS TotalSum 
 	FROM Clerk
 		INNER JOIN [User] ON [User].UserName = Clerk.UserName
-		INNER JOIN (
+		LEFT OUTER JOIN (
 			SELECT Reservation.PickupClerkId, count(Reservation.PickupClerkId) AS TotalPickups FROM Reservation
 
 			WHERE DATEPART(mm, Reservation.StartDate) = @Month AND DATEPART(yyyy, Reservation.StartDate) = @Year
 			GROUP BY Reservation.PickupClerkId
 		) AS Pickups on Pickups.PickupClerkId = Clerk.UserName 
-		FULL OUTER JOIN (
+		LEFT OUTER JOIN (
 			SELECT Reservation.DropOffClerkId, count(Reservation.DropOffClerkId) AS TotalDropoffs FROM Reservation
 			WHERE DATEPART(mm, Reservation.StartDate) = @Month AND DATEPART(yyyy, Reservation.StartDate) = @Year
 			GROUP BY Reservation.DropOffClerkId
