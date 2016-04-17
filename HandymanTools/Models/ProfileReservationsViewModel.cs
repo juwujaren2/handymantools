67bec2e394049a7ea55cd5bed89a1e5e92b99c83
Peter Graening <peter.graening@live.com>
2016-04-17 07:46:43 -0500

    Fixed profile reservation view to match requirements doc.

---
+++ HandymanTools/Models/ProfileReservationsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HandymanTools.Models
+{
+    public class ProfileReservationsViewModel
+    {
+        public string Tools { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public string PickupClerk { get; set; }
+        public string DropoffClerk { get; set; }
+        public decimal TotalRental { get; set; }
+        public decimal TotalDeposit { get; set; }
+    }
+}
